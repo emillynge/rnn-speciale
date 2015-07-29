b6c2f4fe74d2d00ddab1aa8b9c146d3f345ebfcb
Emil Sauer Lynger <emillynge24@gmail.com>
2015-07-29 09:27:33 +0200

    Refactor: spelling, string formatting, blank lines, etc

--- QsubTools.py
+++ QsubTools.py
@@ -3,17 +3,17 @@ USER = "s082768@student.dtu.dk"
 WORKDIR = "/zhome/25/2/51526/Speciale/rnn-speciale"
 LOGDIR = "/zhome/25/2/51526/Speciale/rnn-speciale/qsub/logs"
 SERVER_PYTHON_BIN = "/usr/local/gbar/apps/python/2.7.1/bin/python"
-QSUBMANAGER_PORT = 5000
+QSUB_MANAGER_PORT = 5000
 SSH_USERNAME = "s082768"
 SSH_PRIVATE_KEY = "/home/emil/.ssh/id_rsa"
 SSH_HOST = "hpc-fe1.gbar.dtu.dk"
 SSH_PORT = 22
 
-BASEMODULES = {"python": '', "cuda": '', "boost": ''}
+BASE_MODULES = {"python": '', "cuda": '', "boost": ''}
 from subprocess import Popen, PIPE
+
 FNULL = open('/dev/null', 'w')
 import re
-import os
 import Pyro4.socketutil
 from Pyro4 import errors as pyro_errors
 import logging
@@ -21,18 +21,19 @@ import sys
 from copy import copy
 from time import sleep
 
-Pyro4.config.COMMTIMEOUT = 5.0 # without this daemon.close() hangs
+Pyro4.config.COMMTIMEOUT = 5.0  # without this daemon.close() hangs
 
 from collections import namedtuple
+
+
 class InvalidQsubArguments(Exception):
     pass
 
 
-
 def create_logger():
     # create logger with 'spam_application'
-    logger = logging.getLogger('Qsub')
-    logger.setLevel(logging.DEBUG)
+    _logger = logging.getLogger('Qsub')
+    _logger.setLevel(logging.DEBUG)
     # create file handler which logs even debug messages
     fh = logging.FileHandler('logs/qsubs.log')
     fh.setLevel(logging.DEBUG)
@@ -44,11 +45,14 @@ def create_logger():
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
     # add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(ch)
-    return logger
+    _logger.addHandler(fh)
+    _logger.addHandler(ch)
+    return _logger
+
 
 logger = create_logger()
+
+
 class DummyLogger(object):
     def debug(self, *args):
         pass
@@ -62,8 +66,10 @@ class DummyLogger(object):
     def error(self, *args):
         pass
 
+
 def make_tunnel(port, server_host="127.0.0.1"):
     from sshtunnel import SSHTunnelForwarder
+
     server = SSHTunnelForwarder(
         (SSH_HOST, SSH_PORT),
         ssh_username=SSH_USERNAME,
@@ -74,10 +80,12 @@ def make_tunnel(port, server_host="127.0.0.1"):
     server.start()
     return server, server.local_bind_port
 
+
 HPC_Time = namedtuple("Time", ['h', 'm', 's'])
 HPC_Time.__new__.__defaults__ = (0, 0, 0)
 HPC_resources = namedtuple("Resource", ['nodes', 'ppn', 'gpus', 'pvmem', 'vmem'])
 HPC_resources.__new__.__defaults__ = (1, 1, 0, None, None)
+
 
 class QsubClient(object):
     def __init__(self):
@@ -90,20 +98,22 @@ class QsubClient(object):
         self.manager_ssh_server, self.manager_client_port = make_tunnel(5000)
         self.manager = self.get_manager()
         self.manager.is_alive()
-        self.logger.debug("Succesfully connected to Qsub manager on local port %d" % self.manager_client_port)
+        self.logger.debug("Successfully connected to Qsub manager on local port %d".format(self.manager_client_port))
 
     def generator(self, package, module, wallclock, resources, rel_dir="", additional_modules=None):
         return QsubGenerator(self.manager, package, module, wallclock, resources, rel_dir, additional_modules)
 
-    def setup_ssh_server(self):
+    @staticmethod
+    def setup_ssh_server():
         import paramiko
+
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(SSH_HOST, port=SSH_PORT, username=SSH_USERNAME, key_filename=SSH_PRIVATE_KEY)
         return ssh
 
     def isup_manager(self):
-        (i, o, e) = self.ssh.exec_command("cd  %s; %s QsubTools.py isup manager" % (WORKDIR, SERVER_PYTHON_BIN))
+        (i, o, e) = self.ssh.exec_command("cd  %s; %s QsubTools.py isup manager".format(WORKDIR, SERVER_PYTHON_BIN))
         msg = o.readlines()
         self.logger.debug(msg)
         if "False\n" in msg:
@@ -116,24 +126,26 @@ class QsubClient(object):
             raise Exception(e.readlines())
 
     def init_manager(self, retries=0):
-        self.ssh.exec_command("cd  %s; %s QsubTools.py init manager" % (WORKDIR, SERVER_PYTHON_BIN), timeout=4)
+        self.ssh.exec_command("cd  %s; %s QsubTools.py init manager".format(WORKDIR, SERVER_PYTHON_BIN), timeout=4)
         sleep(4)
         if not self.isup_manager():
             self.logger.info("Manager still not up after init")
             if retries > self.max_retry:
-                self.logger.error("Giving up on initializing manager after %d tries" % self.max_retry)
+                self.logger.error("Giving up on initializing manager after %d tries".format(self.max_retry))
             else:
-                self.init_manager(retries=retries+1)
+                self.init_manager(retries=retries + 1)
 
-    def get_manager(self, retries=0):
-        return Pyro4.Proxy("PYRO:qsub.manager@localhost:%d" % self.manager_client_port)
+    def get_manager(self):
+        return Pyro4.Proxy("PYRO:qsub.manager@localhost:%d".format(self.manager_client_port))
+
 
 def init_manager():
-    daemon = Pyro4.Daemon(port=QSUBMANAGER_PORT)
+    daemon = Pyro4.Daemon(port=QSUB_MANAGER_PORT)
     manager = QsubManager()
     daemon.register(manager, "qsub.manager")
     print "putting manager in request loop"
     daemon.requestLoop(loopCondition=manager.is_alive)
+
 
 def isup_manager():
     manager = Pyro4.Proxy("PYRO:qsub.manager@localhost:5000")
@@ -143,10 +155,10 @@ def isup_manager():
     except pyro_errors.CommunicationError:
         print False
 
+
 class QsubManager(object):
     def __init__(self):
         self._available_modules = self.get_available_modules()
-        active_qsubs = dict()
         self.running = True
 
     def available_modules(self):
@@ -183,7 +195,7 @@ class QsubGenerator(object):
         self.available_modules = qsub_manager.available_modules()
         self.resources = None
         self.wc_time = None
-        self.modules = copy(BASEMODULES)
+        self.modules = copy(BASE_MODULES)
         self.base_dir = WORKDIR
         self.rel_dir = rel_dir
         self.package = package
@@ -191,21 +203,21 @@ class QsubGenerator(object):
 
         self.resources = resources
         self.wc_time = wallclock
-    #
-    # def set_parameters(self, hours=0, minutes=0, seconds=0, nodes=1, processors_per_node=1, gpus=0,
-    #                    memory_per_process=None, memory_total=None, ):
-    #     if not any([hours, minutes, seconds]):
-    #         raise InvalidQsubArguments('No wall clock time assigned to job: %d:%d:%d' % (hours, minutes, seconds))
-    #
-    #     if nodes < 1 or processors_per_node < 1:
-    #         raise InvalidQsubArguments('A job must have at least 1 node and 1 processor')
-    #
-    #     self.resources = HPC_resources(nodes=nodes, ppn=processors_per_node, gpus=gpus, pvmem=memory_per_process,
-    #                                    vmem=memory_total)
-    #     self.wc_time = HPC_Time(h=hours, m=minutes, s=seconds)
-    #
-    #     if not os.path.exists(self.work_dir):
-    #         raise InvalidQsubArguments("Work directory %s doesn't exist." % self.work_dir)
+        #
+        # def set_parameters(self, hours=0, minutes=0, seconds=0, nodes=1, processors_per_node=1, gpus=0,
+        # memory_per_process=None, memory_total=None, ):
+        # if not any([hours, minutes, seconds]):
+        #         raise InvalidQsubArguments('No wall clock time assigned to job: %d:%d:%d' % (hours, minutes, seconds))
+        #
+        #     if nodes < 1 or processors_per_node < 1:
+        #         raise InvalidQsubArguments('A job must have at least 1 node and 1 processor')
+        #
+        #     self.resources = HPC_resources(nodes=nodes, ppn=processors_per_node, gpus=gpus, pvmem=memory_per_process,
+        #                                    vmem=memory_total)
+        #     self.wc_time = HPC_Time(h=hours, m=minutes, s=seconds)
+        #
+        #     if not os.path.exists(self.work_dir):
+        #         raise InvalidQsubArguments("Work directory %s doesn't exist.".format(self.work_dir)
         if additional_modules:
             self.modules.update(additional_modules)
         self.check_modules()
@@ -217,9 +229,10 @@ class QsubGenerator(object):
     def check_modules(self):
         for (module, version) in self.modules.iteritems():
             if module not in self.available_modules:
-                raise InvalidQsubArguments("Required module %s is not available" % module)
+                raise InvalidQsubArguments("Required module %s is not available".format(module))
             if version and version not in self.available_modules[module]:
-                raise InvalidQsubArguments("Required module version %s is not available for module %" % (version, module))
+                raise InvalidQsubArguments("Required module version %s is not available for module %s".format(version,
+                                                                                                              module))
 
     def qsub(self):
         pass
@@ -230,8 +243,9 @@ class QsubGenerator(object):
     def read_logs(self):
         pass
 
+
 if __name__ == "__main__":
-    parameters  = sys.argv[1:]
+    parameters = sys.argv[1:]
     print parameters
     if parameters[0] == 'init':
         if parameters[1] == 'manager':
