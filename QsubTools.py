247f07cd7e871762f5aaed29520072d297bf0434
Emil Sauer Lynge <emillynge24@gmail.com>
2015-08-06 19:29:20 +0200

    clean logfiles on submission request

--- QsubTools.py
+++ QsubTools.py
@@ -314,9 +314,10 @@ class QsubManager(object):
         return self.latest_sub_id, self.logfile(self.latest_sub_id)
 
     def clean_qsub(self, sub_id):
-        p = Popen(['rm', '-f', self.logfile(sub_id) + '.*'], stdout=PIPE, stderr=PIPE)
+        cmd = 'rm -f ' + self.logfile(sub_id) + '.*'
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
         self.logger.debug('removing logfiles with command {1}:\n\t{0}'.format(p.communicate(),
-                                                                              ['rm', '-f', self.logfile(sub_id) + '.*']))
+                                                                              cmd))
         Popen(['rm', '-f', self.subid2sh(sub_id)])
 
     def stage_submission(self, sub_id, script):
