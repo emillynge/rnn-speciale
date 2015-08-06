00fa649cce12a9193f0187e24935f5fe5fce4856
Emil Sauer Lynge <emillynge24@gmail.com>
2015-08-06 16:09:37 +0200

    add classes to use as scripts for remote execution

---
+++ script-classes.py
@@ -0,0 +1,17 @@
+__author__ = 'emil'
+from imp import load_source
+from QsubTools import InvalidUserInput
+class CUDA_tests(object):
+    def __init__(self):
+        self.test_classes = {'math': load_source('cumath', '/pycuda-git/test_cumath.py').TestMath,
+                             'driver': load_source('cumath', '/pycuda-git/test_driver.py').TestDriver,
+                             'gpuarray': load_source('cumath', '/pycuda-git/test_driver.py').TestGPUArray}
+
+    def test_class(self, case):
+        if case not in self.test_classes:
+            raise InvalidUserInput('test case not found', 'case', self.test_classes.keys(), case)
+
+        tests = [t for t in self.test_classes[case].__dict__.keys() if 'test' in t]
+        test_obj = self.test_classes[case]()
+        for test in tests:
+            getattr(test_obj, test).__call__()
